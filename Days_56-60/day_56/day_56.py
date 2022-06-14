7180c7d4d058e330559ecd342cc0d3d759b38b3e
Parker Rosenbauer <parkerrosenbauer@gmail.com>
2022-06-14 09:39:22 -0500

    Update day_56.py

--- Days_56-60/day_56/day_56.py
+++ Days_56-60/day_56/day_56.py
@@ -1,5 +1,10 @@
 # Day 56 of 100 Days of Code Challenge
 # Virtual Business Card
+
+# This day involved learning how to incorperate html and css style sheets into flask, 
+# first testing with previous html/css projects, then looking at using free online resources. I did not upload all of the resources from the online template
+# as there were a lot
+
 # Checkout the preview image to see what it looks like
 
 from flask import Flask, render_template
